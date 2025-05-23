23d272cdda45b986f30775b8ae1bd83d35535a72
Yordano Pascual <yordanoweb@gmail.com>
2025-05-23 14:27:20 -0300

    feat: integrate colorscheme, modify ip addr and cpu temp
    - Replace hardcoded colors in ybar.sh with variables for colorscheme integration
    - modify ip address retrieval to select the first available address
    - format cpu temperature output

--- config/i3/ybar.sh
+++ config/i3/ybar.sh
@@ -44,6 +44,7 @@ send_battery_alert() {
     fi
 }
 
+
 XRES="$HOME/.Xresources"
 
 # Function to get default if not found
@@ -114,12 +115,13 @@ do
   hour=$(date "+%H:%M:%S")
   today=$(date "+%a, %b/%d")
 
-  ip_addr=$(nmcli | grep inet4 | awk '{print $2}' | grep -v "^127")
+  ip_addr=$(nmcli | grep inet4 | awk '{print $2}' | grep -v "^127" | head -n 1)
   if [ $(ip link show | grep --color=never -c "state UP") -eq 0 ];
   then
     ip_addr="offline"
   fi
 
+  # battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -o -E --color=never '[0-9]+%')
   audio=$(amixer get Master | grep -E -o --color=never '[0-9]+%' | head -n 1)
   bright=$(brightnessctl | grep Current | grep -o -E --color=never '[0-9]+%')
   used_mem=$(free -h | grep Mem | awk '{print $3}' | sed "s/i//" | sed "s/,/./")
@@ -127,7 +129,8 @@ do
   free_data_disk=$(df -h | egrep "\/DATA$" | awk '{print $4}')
   _cpu_load=$(top -b -d1 -n1 | grep Cpu | sed "s/,/./g" | awk 'BEGIN {cpu=0} {cpu+=$2; cpu+=$4} END {printf "%s%%", cpu}')
   cpu_load=$(format_percent "$_cpu_load" 5 " ")
-  cpu_temp=$(sensors | awk '/Pack/ {print $4" "}' | sed "s/+//")
+  _cpu_temp=$(sensors | awk '/Pack/ {print $4" "}' | sed "s/+//")
+  cpu_temp=$(format_percent "$_cpu_temp" 5 " ")
 
   # comment this if you are not in a laptop and remove the corresponding JSON line 
   battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
@@ -136,45 +139,45 @@ do
 
   JSON=$(cat <<EOF
   ,[{
-    "full_text": " 🇨🇺 ", "color": "#ffffff", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": " 🇨🇺 ", "color": "$foreground", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#ea3d3d", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#000000"
+    "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#000000"
   },{
-    "full_text": "   $cpu_load |  $cpu_temp", "color": "#ffffff", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "#ea3d3d"
+    "full_text": "   $cpu_load |   $cpu_temp", "color": "$foreground", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": "", "color": "#3b47aa", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#ea3d3d"
+    "full_text": "", "color": "$color4", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": "   $used_mem ", "color": "#ffffff", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "#3b47aa"
+    "full_text": "   $used_mem ", "color": "$foreground", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "$color4"
   },{
-    "full_text": "", "color": "#5f00af", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#3b47aa"
+    "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color4"
   },{
-    "full_text": "  /: $free_root_disk |   Data: $free_data_disk ", "color": "#ffffff", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "#5f00af"
+    "full_text": "  /: $free_root_disk |   Data: $free_data_disk ", "color": "$foreground", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": "", "color": "#875fd7", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#5f00af"
+    "full_text": "", "color": "$color4", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": "   $audio ", "color": "#ffffff", "background": "#875fd7", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": "   $audio ", "color": "$foreground", "background": "$color4", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#3b47aa", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#875fd7"
+    "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color4"
   },{
-    "full_text": "   $bright ", "color": "#ffffff", "background": "#3b47aa", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": "   $bright ", "color": "$foreground", "background": "$color1", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#ea3d3d", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#3b47aa"
+    "full_text": "", "color": "$color4", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": " $battery_icon  $battery ", "color": "#ffffff", "background": "#ea3d3d", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": " $battery_icon  $battery ", "color": "$foreground", "background": "$color4", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#2c7b39", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#ea3d3d"
+    "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color4"
   },{
-    "full_text": "   $ip_addr ", "color": "#ffffff", "background": "#2c7b39", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": "   $ip_addr ", "color": "$foreground", "background": "$color1", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#875fd7", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#2c7b39"
+    "full_text": "", "color": "$color4", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": "   $today ", "color": "#ffffff", "background": "#875fd7", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": "   $today ", "color": "$foreground", "background": "$color4", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#5f00af", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#875fd7"
+    "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color4"
   },{
-    "full_text": "   $hour ", "color": "#ffffff", "background": "#5f00af", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": "   $hour ", "color": "$foreground", "background": "$color1", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
-    "full_text": "", "color": "#000000", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "#5f00af"
+    "full_text": "", "color": "#000000", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   }]
 EOF
 )
