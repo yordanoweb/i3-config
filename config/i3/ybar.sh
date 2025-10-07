a1ddd536c88b158d7ba6337fa86492f8a451ae37
Yordano Pascual <yordanoweb@gmail.com>
2025-10-07 07:51:14 -0300

    feat: load colors from ini

--- config/i3/ybar.sh
+++ config/i3/ybar.sh
@@ -18,6 +18,15 @@ format_percent() {
    }'
 }
 
+ac_plugged() {
+  plugged_status=$(upower -d | grep ACAD --color=never -A100 | grep line-power --color=never -A50 | grep online --color=never | awk '{print $2}')
+  if [ "$plugged_status" = "yes" ]; then
+    echo "🔌"  # AC is plugged in
+  else
+    echo "🔋"  # AC is not plugged in
+  fi
+}
+
 get_battery_icon() {
     percent=$(echo "$1" | tr -d '%')
 
@@ -65,63 +74,14 @@ color14=""
 color15=""
 color16=""
 
-# Function to get default if not found
-get_color() {
-    key="$1"
-    value=$(xrdb -query | grep "$key" | awk '{ print $2 }')
-
-    if [ -n "$value" ]; then
-        echo "$value"
-    else
-        case "$key" in
-            background) echo "#0d0f12" ;;
-            foreground) echo "#eeeedf" ;;
-            color0) echo "#0d0f12" ;;
-            color1) echo "#656053" ;;
-            color2) echo "#996A3E" ;;
-            color3) echo "#CC9F66" ;;
-            color4) echo "#7D8082" ;;
-            color5) echo "#BFAE8E" ;;
-            color6) echo "#ECD79F" ;;
-            color7) echo "#eeeedf" ;;
-            color8) echo "#a6a69c" ;;
-            color9) echo "#656053" ;;
-            color10) echo "#996A3E" ;;
-            color11) echo "#CC9F66" ;;
-            color12) echo "#7D8082" ;;
-            color13) echo "#BFAE8E" ;;
-            color14) echo "#ECD79F" ;;
-            color15) echo "#eeeedf" ;;
-            color16) echo "#0d0f12" ;;
-            *) echo "#000000" ;;  # fallback for unknown keys
-        esac
-    fi
-}
-
 assign_colors() {
+    config_file="/media/DATA/yordano/.config/i3/colors.ini"
 
-    # Assigning colors to variables
-    background=$(get_color "background")
-    foreground=$(get_color "foreground")
-    
-    color0=$(get_color "color0")
-    color1=$(get_color "color1")
-    color2=$(get_color "color2")
-    color3=$(get_color "color3")
-    color4=$(get_color "color4")
-    color5=$(get_color "color5")
-    color6=$(get_color "color6")
-    color7=$(get_color "color7")
-    color8=$(get_color "color8")
-    color9=$(get_color "color9")
-    color10=$(get_color "color10")
-    color11=$(get_color "color11")
-    color12=$(get_color "color12")
-    color13=$(get_color "color13")
-    color14=$(get_color "color14")
-    color15=$(get_color "color15")
-    color16=$(get_color "color16")
-
+    if [ -f "$config_file" ]; then
+        # Read key-value pairs from the ini file, filter out section headers, empty lines,
+        # and potential Windows line endings, then use eval to assign them as variables.
+        eval "$(grep -v '^\[.*\]' "$config_file" | sed 's/\r$//' | sed '/^\s*$/d' | awk -F= '{print $1"='\''"$2"'\''"}')"
+    fi
 }
 
 echo '{ "version": 1 }'
@@ -159,6 +119,7 @@ do
   # comment this if you are not in a laptop and remove the corresponding JSON line 
   battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
   battery_icon=$(get_battery_icon "$battery")
+  acdc_icon=$(ac_plugged)
   send_battery_alert "$battery"
 
   JSON=$(cat <<EOF
@@ -187,7 +148,7 @@ do
   },{
     "full_text": "", "color": "$color4", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color1"
   },{
-    "full_text": " $battery_icon  $battery ", "color": "$foreground", "background": "$color4", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
+    "full_text": " $battery_icon  $battery $acdc_icon ", "color": "$foreground", "background": "$color4", "separator": false, "border_right": 0, "border_left": 0, "separator_block_width": 0
   },{
     "full_text": "", "color": "$color1", "separator": false, "border_right": 0, "border_left": 0, "border_bottom": 4, "border_top": 0, "separator_block_width": 0, "background": "$color4"
   },{
